e54f42f693bbe8cf72be2305d5d3dd8baace57a7
hzcscl <192243051@qq.com>
2024-11-18 13:45:52 +0800

    AI调整

--- Frameworks/SpringBoot-AI/src/main/java/com/caicaijava/springbootai/service/AIServiceImpl.java
+++ Frameworks/SpringBoot-AI/src/main/java/com/caicaijava/springbootai/service/AIServiceImpl.java
@@ -1,10 +1,15 @@
 package com.caicaijava.springbootai.service;
 
-import com.alibaba.cloud.ai.tongyi.chat.TongYiChatModel;
+import com.alibaba.cloud.ai.tongyi.image.TongYiImagesModel;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.image.ImagePrompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 /**
  * @author: 菜菜的后端私房菜
@@ -12,18 +17,20 @@ import org.springframework.stereotype.Component;
  * @description:
  */
 @Component
-public class AIServiceImpl implements AIService{
+public class AIServiceImpl implements AIService {
 
-    private final TongYiChatModel tongYiChatModel;
+    private final ChatModel chatModel;
+
 
     @Autowired
-    public AIServiceImpl(TongYiChatModel TongYiChatClient) {
-        this.tongYiChatModel = TongYiChatClient;
+    public AIServiceImpl(ChatModel TongYiChatClient) {
+        this.chatModel = TongYiChatClient;
     }
 
     @Override
     public String chat(String msg) {
         Prompt prompt = new Prompt(new UserMessage(msg));
-        return tongYiChatModel.call(prompt).getResult().getOutput().getContent();
+        return chatModel.call(prompt).getResult().getOutput().getContent();
     }
+
 }
